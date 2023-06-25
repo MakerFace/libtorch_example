6cc23dd74a12d96ec8ea1ed2474ada30b19b58cd
MakerFace <chenqi@imudges.com>
2023-06-25 11:50:38 +0800

    use torch::load load TorchScript

--- include/resnet101_impl.h
+++ include/resnet101_impl.h
@@ -1,26 +1,53 @@
 #ifndef RESNET_101_IMPL_H
 #define RESNET_101_IMPL_H
 
+#include <torch/nn/modules/activation.h>
+#include <torch/nn/modules/pooling.h>
 #include <torch/nn/pimpl.h>
 #include <torch/torch.h>
+#include <cstdint>
+#include <vector>
+
+struct TORCH_API ResNet101Options {
+  ResNet101Options(std::vector<int> layers, int num_classes);
+
+  TORCH_ARG(std::vector<int>, layers);
+
+  TORCH_ARG(int, expansion);
+
+  TORCH_ARG(bool, is_basic);
+
+  TORCH_ARG(int64_t, inplanes);
+
+  TORCH_ARG(int, base_width);
+
+  TORCH_ARG(int, num_classes);
+
+  TORCH_ARG(int, num_channels);
+
+  TORCH_ARG(std::string, model_type);
+
+  TORCH_ARG(int, groups);
+
+  TORCH_ARG(int, width_per_group);
+};
 
 class ResNet101Impl : public torch::nn::Module {
  public:
-  ResNet101Impl() {}
-  ResNet101Impl(std::vector<int> layers, int num_classes = 1000,
-                std::string model_type = "resnet18", int groups = 1,
-                int width_per_group = 64);
+  // ResNet101Impl() {}
+  ResNet101Impl(const ResNet101Options& options_);
   torch::Tensor forward(torch::Tensor x);
   std::vector<torch::Tensor> features(torch::Tensor x);
   torch::nn::Sequential _make_layer(int64_t planes, int64_t blocks,
                                     int64_t stride = 1);
 
  private:
-  int expansion = 1;
-  bool is_basic = true;
-  int64_t inplanes = 64;
-  int groups = 1;
-  int base_width = 64;
+  // int expansion = 1;  // neck::expansion
+  // bool is_basic = true;
+  // int64_t inplanes = 64;
+  // int groups = 1;
+  // int base_width = 64;
+  ResNet101Options options;
   torch::nn::Conv2d conv1 = nullptr;
   torch::nn::BatchNorm2d bn1 = nullptr;
   torch::nn::Sequential layer1 = nullptr;
